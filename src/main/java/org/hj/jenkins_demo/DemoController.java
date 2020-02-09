de598d9be73cf1c0a7142dd14bb9a567fd721fb0
smile2333 <hj980959100@gmail.com>
2020-02-09 13:55:15 +0800

    using springMVC to test jenkins via gradle building

---
+++ src/main/java/org/hj/jenkins_demo/DemoController.java
@@ -0,0 +1,12 @@
+package org.hj.jenkins_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("demo")
+    public String demo(){
+        return "hello world";
+    }
+}
