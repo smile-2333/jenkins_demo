336559f1fb9682a060b65f480218a91bbda9d72c
smile2333 <hj980959100@gmail.com>
2020-02-10 01:09:17 +0800

    update

--- src/main/java/org/hj/jenkins_demo/DemoController.java
+++ src/main/java/org/hj/jenkins_demo/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("demo")
     public String demo(){
-        return "hello world12";
+        return "hello world123";
     }
 }
